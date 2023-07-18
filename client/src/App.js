8d8dadc8a88841a6ad480cceb6bd65f2d6045690
Ethan <ethanliu50@gmail.com>
2023-07-17 23:28:10 -0500

    converted js to react components and added routes for login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,21 @@
 import './App.css';
+import Post from './Post';
+import Header from './Header';
+import {Routes, Route} from "react-router-dom"
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post"> 
-        <div className="image"><img src="https://images.squarespace-cdn.com/content/v1/5acea5f9f2e6b1da315acf14/1687814629484-Y0UHDWRKYAPA5IQC8B0O/g-Un-vHvg5ezU-unsplash.jpg?format=750w" alt="Google Building" /></div>
-        <div className="texts">
-          <h2>What makes Google's Organizational Culture Stand Out?</h2>
-          <p className="info">
-            <a href="" className="author">Ethan Liu</a>
-            <time>2023-07-16</time>
-          </p>
-          <p className="summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nemo alias cupiditate totam iure neque sed ratione, aspernatur pariatur suscipit ad aut quos consequuntur impedit magni error, architecto vero aliquid?</p>
-        </div>
-      </div>
-      <div className="post"> 
-        <div className="image"><img src="https://images.squarespace-cdn.com/content/v1/5acea5f9f2e6b1da315acf14/1687814629484-Y0UHDWRKYAPA5IQC8B0O/g-Un-vHvg5ezU-unsplash.jpg?format=750w" alt="Google Building" /></div>
-        <div className="texts">
-          <h2>What makes Google's Organizational Culture Stand Out?</h2>
-          <p className="info">
-            <a href="" className="author">Ethan Liu</a>
-            <time>2023-07-16</time>
-          </p>
-          <p className="summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nemo alias cupiditate totam iure neque sed ratione, aspernatur pariatur suscipit ad aut quos consequuntur impedit magni error, architecto vero aliquid?</p>
-        </div>
-      </div>
-      <div className="post"> 
-        <div className="image"><img src="https://images.squarespace-cdn.com/content/v1/5acea5f9f2e6b1da315acf14/1687814629484-Y0UHDWRKYAPA5IQC8B0O/g-Un-vHvg5ezU-unsplash.jpg?format=750w" alt="Google Building" /></div>
-        <div className="texts">
-          <h2>What makes Google's Organizational Culture Stand Out?</h2>
-          <p className="info">
-            <a href="" className="author">Ethan Liu</a>
-            <time>2023-07-16</time>
-          </p>
-          <p className="summary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nemo alias cupiditate totam iure neque sed ratione, aspernatur pariatur suscipit ad aut quos consequuntur impedit magni error, architecto vero aliquid?</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<LoginPage />} />  
+        <Route path={'/register'} element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
